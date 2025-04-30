d5aadf1cbc31be2cde8efbb4bf2b1c8d12b65150
Tomofumi Fujii <toragon244802@gmail.com>
2025-04-30 12:42:03 +0900

    First commit 04/30

---
+++ app/src/main/java/jp/ac/gifu_u/i6/fujii/myapplication_s/MainActivity.java
@@ -0,0 +1,35 @@
+package jp.ac.gifu_u.i6.fujii.myapplication_s;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this, "アプリを終了しました", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        /*
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });*/
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(this);
+    }
+}
